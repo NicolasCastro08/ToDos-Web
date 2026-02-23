c2b0ee6d71ac9146fb6d1e3cf8627b8b4c18b3a1
NicolasCastro08 <nicolasferreia@gmail.com>
2026-02-23 13:28:18 -0300

    Criado o projeto e iniciada a camada de dados 2

--- ToDoPlataform/Models/ToDo.cs
+++ ToDoPlataform/Models/ToDo.cs
@@ -1,28 +1,33 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ToDoPlataform.Models;
+namespace ToDoPlatform.Models;
 
 [Table("todos")]
-
 public class ToDo
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-
-    public int Id {get; set; }
+    public int Id { get; set; }
 
     [Required]
-    
-    public string UserId { get; set; }
+    public string UserId { get; set; } // usuário no banco
+    [ForeignKey(nameof(UserId))]
 
-
-    public AppUser User { get; set; }
+    public AppUser User { get; set; } //propriedade de navegação(acesso as info. do usuário)
 
     [StringLength(100)]
-
+    [Display(Name = "Título")]
     public string Title { get; set; }
 
+    [Display(Name = "Descrição")]
+    public string Description { get; set; }
+
+    public bool Done { get; set; } = false; //tarefa concluida ou não
+
+    public DateTime CreateAt { get; set; } = DateTime.Now;
+
+}
     
 
 
@@ -30,4 +35,3 @@ public class ToDo
 
 
 
-}
