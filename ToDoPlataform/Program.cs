cfeb478d7353ccd7287cf5d23a94efc26e5e01a4
NicolasCastro08 <nicolasferreia@gmail.com>
2026-03-02 12:25:14 -0300

    configurado serviços

--- ToDoPlataform/Program.cs
+++ ToDoPlataform/Program.cs
@@ -1,9 +1,39 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ToDoPlatform.Data;
+using ToDoPlatform.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Serviço de conexão com o banco de dados
+string conexao = builder .Configuration.GetConnectionString("Conexao");
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySQL(conexao)
+);
+
+// Serviço de configuração do Identity - Gestão de Usuários
+builder.Services.AddIdentity<AppUser, IdentityRole>(
+    opt =>
+    {
+        opt.User.RequireUniqueEmail = true;
+        opt.SignIn.RequireConfirmedAccount = false;
+    }
+
+)
+.AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+// Garante a existência do banco
+using (var scope = app.Services.CreateScope())
+{
+    var DbContext = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -16,6 +46,7 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
