bfb5c2f8125c10d24e881422f56527fb6e38efd0
NicolasCastro08 <nicolasferreia@gmail.com>
2026-04-17 12:28:11 -0300

    projeto finalizado (apenas ajustando alguns erros)

--- ToDoPlatform/Controllers/ AccountController.cs
+++ ToDoPlatform/Controllers/ AccountController.cs
@@ -49,6 +49,37 @@ public class AccountController : Controller
         await _userService.Logout();
         return RedirectToAction("Login", "Account");
     }
-    public IActionResult Register() => View();
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Register(RegisterVM registerVM)
+    {
+        if (ModelState.IsValid && registerVM.Terms)
+        {
+            var result = await _userService.Register(registerVM);
+            if (result.Count == 0)
+            {
+                TempData["Success"] = "Conta criada com sucesso!";
+                return Redirect("/");
+            }
+            else
+            {
+                foreach (var error in result)
+                    TempData["Failure"] += error + "\n";
+            }
+        }
+        else
+        {
+            TempData["Failure"] = "Dados inválidos. Verifique os campos preenchidos.";
+        }
+
+        return View(registerVM);
+    }
     public IActionResult Profile() => View();
 }
